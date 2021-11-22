4cbf751eb899a0bd0e801eaaa4e72161fdd1ed7b
OX-S <finn.kliewer@gmail.com>
2021-11-21 22:08:40 -0500

    Made changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
 from views import views
 
 app = Flask(__name__)
 app.secret_key = "How We Doin?"
 app.register_blueprint(views, url_preifx ="/")
+
+
 if __name__ == '__main__':
-    app.run(debug = True, port = 70, host = '0.0.0.0')
+    app.run(debug=True, port=70, host='0.0.0.0')
--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, url_for, redirect, request
+
 views = Blueprint(__name__, "views")
+
+
 @views.route("/")
 def home():
     return render_template("home.html")
