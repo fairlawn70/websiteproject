41dd885ffc71d9b59e2334f9f0de66da6db5360a
OX-S <finn.kliewer@gmail.com>
2021-12-01 00:35:37 -0500

    did meun and made it working

--- views.py
+++ views.py
@@ -6,3 +6,34 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     return render_template("home.html")
+
+
+@views.route("/home")
+def landing():
+    return redirect(url_for('views.home'))
+
+
+
+@views.route("/academic")
+def academic():
+    return render_template("academic.html")
+
+
+@views.route("/artphoto")
+def artphoto():
+    return render_template("artphoto.html")
+
+
+@views.route("/athletic")
+def athletic():
+    return render_template("athletic.html")
+
+
+@views.route("/mybook")
+def mybook():
+    return render_template("book.html")
+
+
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
